cc82393f56f14da128ebe5722eaa421467531fb9
LajuPDBE <arnoldlaju@gmail.com>
2022-09-23 16:05:09 +0300

    initial commit

---
+++ src/test/java/com/rols/educ/RolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rols.educ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
