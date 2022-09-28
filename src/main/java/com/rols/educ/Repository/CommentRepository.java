0d560c00c8c3c64c923ee7e33f72be0a7a31a09c
Chris-Aplexy <mcaplexyc@gmail.com>
2022-09-28 15:12:05 +0300

    first commit

--- src/main/java/com/rols/educ/Repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rols.educ.Repository;
-
-import com.rols.educ.Model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
